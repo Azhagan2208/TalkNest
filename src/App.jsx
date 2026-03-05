0714aec0ae80018be3a6c865b379514e7185de00
AzhaganNJ <azhagannatrajan2006@gmail.com>
2026-03-05 11:35:11 +0530

    Remove unused import of CompleteSubmission component from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { RouterProvider } from 'react-router-dom'
 import routes from './router/routes.jsx'
-import CompleteSubmission from './pages/CompleteSubmission.jsx'
 
 const App = () => {
   return (
